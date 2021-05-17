0a4b9ebb6cc2a581cf9fbbf6c20b411293dfab0b
Blas <bcarofile@gmail.com>
2021-05-17 14:54:30 -0300

    agregado https a la api

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -3,7 +3,7 @@
 export const getGifs = async(category) => {
 
     //utilizamos el encode para sacar los espacios y transformarlos en data de la url
-    const url = `http://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=ZPJ6nf1iq0ceG4wNI3fpISF8sBONuKhQ`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=ZPJ6nf1iq0ceG4wNI3fpISF8sBONuKhQ`;
     const respuesta = await fetch( url );
     // Desestructuro el arreglo
     const { data } = await respuesta.json();
